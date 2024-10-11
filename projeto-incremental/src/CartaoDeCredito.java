0d832b5143e5827c800347505f6eabd8eefc6616
Eliane Isadora Faveron Maciel <eliane.faveron@gmail.com>
2024-10-11 14:24:14 +0000

    Parte 1 - exercicio incremental

---
+++ projeto-incremental/src/CartaoDeCredito.java
@@ -0,0 +1,24 @@
+public class CartaoDeCredito {
+    int numero;
+    String nomeTitular;
+    String cpf;
+    double limite;
+    double totalFatura;
+
+    public double consultarLimite(){
+        return limite;
+    }
+
+    public double consultarTotalFatura(){
+        return totalFatura;
+    }
+
+    public void realizarCompra(double valor) {
+        if (valor <= this.limite){
+            this.limite -= valor;
+            this.totalFatura += valor;
+        } else {
+            System.out.println("Você não possui limite necessario para essa compra");
+        }
+    }
+}
